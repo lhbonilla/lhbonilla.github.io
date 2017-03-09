57f2490b5b0869682f79abc2e9f0ff21d2b49692
lhbonilla <cortes7325@gmail.com>
2017-03-09 09:31:19 -0500

    Add files via upload
    Update page with latest oscar

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<?php
+  $mail="pineappcomco@gmail.com";
+  $nombre = $_POST["nombre"];
+  $email = $_POST["email"];
+  $phone = $_POST["phone"];
+  $mensaje = $_POST["mensaje"];
+  $thank="index-3.html";
+  $message = "
+  nombre:".$nombre."
+  email:".$email."
+  phone:".$phone."
+  mensaje:".$mensaje."";
+  if (mail ($mail,"contactenos",$message)) Header ("location: $thank" );
+  ?>
